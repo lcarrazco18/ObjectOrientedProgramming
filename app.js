9e71213a4532b0cf7919422e89b097e99b4feb3d
Luis Carrazco <luiscarrazco@gmail.com>
2019-06-05 16:58:55 -0600

    Prototypical Inheritance

--- app.js
+++ app.js
@@ -26,6 +26,7 @@
 
 // const brad = new Person('Brad', '9-10-1981');
 // console.log(brad.calculateAge());
+/////////////////////////////////////////////////////////////
 
 // // Built in Constructors
 
@@ -72,48 +73,95 @@
 // // Regular Expressions
 // const re1 = /\w+/;
 // const re2 = new RegExp("\\w+");
+///////////////////////////////////////////////////////////
 
 // Object.prototype
 // Person.prototype
-function Person(firstName, lastName, dob) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.birthday = new Date(dob);
+// function Person(firstName, lastName, dob) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.birthday = new Date(dob);
   //   this.calculateAge = function() {
   //     const diff = Date.now() - this.birthday.getTime();
   //     const ageDate = new Date(diff);
   //     return Math.abs(ageDate.getUTCFullYear() - 1970);
   //   }
-}
+// }
 
 // Calculate age
-Person.prototype.calculateAge = function(){
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  }
+// Person.prototype.calculateAge = function(){
+//     const diff = Date.now() - this.birthday.getTime();
+//     const ageDate = new Date(diff);
+//     return Math.abs(ageDate.getUTCFullYear() - 1970);
+//   }
 
-  // Get full name
-  Person.prototype.getFullName = function () {
-    return `${this.firstName} ${this.lastName}`;
-  }
+//   // Get full name
+//   Person.prototype.getFullName = function () {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
 
-  // Gets Married
-  Person.prototype.getsMarried = function(newLastName){
-    this.lastName = newLastName;
-  }
+//   // Gets Married
+//   Person.prototype.getsMarried = function(newLastName){
+//     this.lastName = newLastName;
+//   }
 
-const john = new Person('John', 'Doe', '8-12-90');
-const mary = new Person('Mary', 'Johnson', 'March 20 1978');
+// const john = new Person('John', 'Doe', '8-12-90');
+// const mary = new Person('Mary', 'Johnson', 'March 20 1978');
 
-console.log(mary);
+// console.log(mary);
 
-console.log(john.calculateAge());
+// console.log(john.calculateAge());
 
-console.log(mary.getFullName());
+// console.log(mary.getFullName());
 
-mary.getsMarried('Smith');
+// mary.getsMarried('Smith');
 
-console.log(mary.getFullName());
+// console.log(mary.getFullName());
 
-console.log(mary.hasOwnProperty('getFullName'));
+// console.log(mary.hasOwnProperty('getFullName'));
+//////////////////////////////////////////////////
+
+
+// Prototypical Inheritance
+
+// Person constructor
+
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+// Greeting
+Person.prototype.greeting = function(){
+  return `Hello there ${this.firstName} ${this.lastName}`;
+}
+
+const person1 = new Person('John', 'Doe');
+// console.log(person1.greeting());
+
+// Customer constructor
+function Customer(firstName, lastName, phone, membership) {
+  Person.call(this, firstName, lastName);
+
+  this.phone = phone;
+  this.membership = membership;
+}
+
+// Inherit the Person prototype methods
+Customer.prototype = Object.create(Person.prototype);
+
+// Make customer.prototype return Customer()
+Customer.prototype.constructor = Customer;
+
+// Create customer
+const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'Standard');
+
+console.log(customer1);
+
+// Customer greeting
+Customer.prototype.greeting = function(){
+  return `Hello there ${this.firstName} ${this.lastName} Welcome to our company`;
+}
+
+console.log(customer1.greeting());
+
